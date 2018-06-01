f6b5d9e053b967c86aa886eee20b8794042e1b6a
Antoaneta Damyanova <adamyanova@gmail.com>
2018-06-01 14:29:41 +0200

    iter all clients

--- qa/tasks/s3tests_go.py
+++ qa/tasks/s3tests_go.py
@@ -202,7 +202,7 @@ class S3tests_go(Task):
         for (host, roles) in cluster.remotes.iteritems():
             log.info("S3 Tests Go: s3tests_conf is {s3cfg}".format(s3cfg = s3tests_conf))
             for (section, user) in users.iteritems():
-                _config_user(s3tests_conf, section, '{user}.{host}'.format(user=user, host=host))
+                self._config_user(s3tests_conf, section, '{user}.{host}'.format(user=user, host=host))
                 log.debug('S3 Tests Go: Creating user {user} on {host}'.format(user=s3tests_conf[section]['user_id'], host=host))
                 cluster_name, daemon_type, client_id = teuthology.split_role(host)
                 client_with_id = daemon_type + '.' + client_id
@@ -290,9 +290,9 @@ class S3tests_go(Task):
                         for id_ in teuthology.all_roles_of_type(self.ctx.cluster, 'client')]
                         # the iterator is needed because all_roles_of_type() is a generator
         log.info("S3 Tests Go: List all_clients: {clts}".format(clts=all_clients))
-        clients = 'client.0'
+        # clients = {'client.0'}
         s3tests_conf = {}
-        for client in clients:
+        for client in all_clients:
             log.info("S3 Tests Go: config for client {clt}".format(clt=client))
             endpoint = self.ctx.rgw.role_endpoints.get('client.0')
             # assert endpoint, 'S3 Tests Go: no rgw endpoint for {}'.format(client)
