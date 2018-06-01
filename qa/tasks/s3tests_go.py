5f7429e3bec89dffc8423de529450250c6ab50d1
Antoaneta Damyanova <adamyanova@gmail.com>
2018-06-01 21:40:35 +0200

    double quotes

--- qa/tasks/s3tests_go.py
+++ qa/tasks/s3tests_go.py
@@ -189,22 +189,23 @@ class S3tests_go(Task):
         """
         log.info("S3 Tests Go: s3tests_conf[{sect}] is {s3cfg}".format(sect=section, s3cfg = s3tests_conf[section]))
         log.debug('S3 Tests Go: Setion, User = {sect}, {user}'.format(sect=section, user=user))
-        s3tests_conf[section].setdefault('user_id', user)
-        s3tests_conf[section].setdefault('email', '{user}_test@test.test'.format(user=user))
-        s3tests_conf[section].setdefault('display_name', 'Ms. {user}'.format(user=user))
+        s3tests_conf[section].setdefault('user_id', '"{user}"'.format(user=user))
+        s3tests_conf[section].setdefault('email', '"{user}_test@test.test"'.format(user=user))
+        s3tests_conf[section].setdefault('display_name', '"Ms. {user}"'.format(user=user))
         if section is 's3main':
-            s3tests_conf[section].setdefault('access_key', '0555b35654ad1656d804')
-            s3tests_conf[section].setdefault('secret_key', 'h7GhxuBLTrlhVUyxSPUKUV8r/2EI4ngqJxD7iBdBYLhwluN30JaT3Q==')
+            s3tests_conf[section].setdefault('access_key', '"0555b35654ad1656d804"')
+            s3tests_conf[section].setdefault('secret_key', '"h7GhxuBLTrlhVUyxSPUKUV8r/2EI4ngqJxD7iBdBYLhwluN30JaT3Q=="')
         else:
-            s3tests_conf[section].setdefault('access_key', ''.join(random.choice(string.ascii_uppercase) for i in range(20)))
-            s3tests_conf[section].setdefault('secret_key', base64.b64encode(os.urandom(40)))
+            s3tests_conf[section].setdefault('access_key', '""'.join(random.choice(string.ascii_uppercase) for i in range(20)))
+            skey = base64.b64encode(os.urandom(40))
+            s3tests_conf[section].setdefault('secret_key', '"{key}"'.format(key=skey))
         # s3tests_conf[section].setdefault('totp_serial', ''.join(random.choice(string.digits) for i in range(10)))
         # s3tests_conf[section].setdefault('totp_seed', base64.b32encode(os.urandom(40)))
         # s3tests_conf[section].setdefault('totp_seconds', '5')
-        s3tests_conf[section].setdefault('kmskeyid', 'barbican_key_id')
-        s3tests_conf[section].setdefault('bucket', 'bucket1')
-        s3tests_conf[section].setdefault('region', 'us-east-1')
-        s3tests_conf[section].setdefault('SSE', 'AES256')
+        s3tests_conf[section].setdefault('kmskeyid', '"barbican_key_id"')
+        s3tests_conf[section].setdefault('bucket', '"bucket1"')
+        s3tests_conf[section].setdefault('region', '"us-east-1"')
+        s3tests_conf[section].setdefault('SSE', '"AES256"')
 
     def create_users(self):
         """
@@ -299,27 +300,27 @@ class S3tests_go(Task):
                 infile={
                     'DEFAULT':
                         {
-                        'host'      : endpoint.hostname,
-                        'port'      : endpoint.port,
-                        'is_secure' : 'yes' if endpoint.cert else 'no',
+                        'host'      : '"{ep}"'.format(ep=endpoint.hostname),
+                        'port'      : '"{ep}"'.format(ep=endpoint.port),
+                        'is_secure' : '"yes"' if endpoint.cert else '"no"',
                         },
                     'fixtures' : {
-                        'bucket_prefix' : 'joannah' 
+                        'bucket_prefix' : '"joannah"' 
                         },
                     's3main'  : {
-                        'host'      : endpoint.hostname,
-                        'port'      : endpoint.port,
-                        'is_secure' : 'yes' if endpoint.cert else 'no',
+                        'host'      : '"{ep}"'.format(ep=endpoint.hostname),
+                        'port'      : '"{ep}"'.format(ep=endpoint.port),
+                        'is_secure' : '"yes"' if endpoint.cert else '"no"',
                     },
                     's3alt'   : {
-                        'host'      : endpoint.hostname,
-                        'port'      : endpoint.port,
-                        'is_secure' : 'yes' if endpoint.cert else 'no', 
+                        'host'      : '"{ep}"'.format(ep=endpoint.hostname),
+                        'port'      : '"{ep}"'.format(ep=endpoint.port),
+                        'is_secure' : '"yes"' if endpoint.cert else '"no"',
                     },
                     's3tenant': {
-                        'host'      : endpoint.hostname,
-                        'port'      : endpoint.port,
-                        'is_secure' : 'yes' if endpoint.cert else 'no',
+                        'host'      : '"{ep}"'.format(ep=endpoint.hostname),
+                        'port'      : '"{ep}"'.format(ep=endpoint.port),
+                        'is_secure' : '"yes"' if endpoint.cert else '"no"',
                     },
                     }
                 )
