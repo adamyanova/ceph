e41f84a19770d5b137ad89c4eee62b5c8d53e133
Antoaneta Damyanova <adamyanova@gmail.com>
2018-06-04 13:19:03 +0200

    change config dir

--- qa/tasks/s3tests_go.py
+++ qa/tasks/s3tests_go.py
@@ -214,13 +214,13 @@ class S3tests_go(Task):
                 for id_ in teuthology.all_roles_of_type(self.ctx.cluster, 'client')]
         users = {'s3main': 'tester', 's3alt': 'johndoe'}
         # s3tests_conf = self.s3tests_skelethon_config()
-        s3tests_conf = teuthology.config_file('{tdir}/s3-tests/config.toml.sample'.format(tdir=testdir))
+        s3tests_conf = teuthology.config_file('/home/adamyanova/src/go_s3tests/config.toml.sample'.format(tdir=testdir))
         for client in all_clients:
             # log.info("S3 Tests Go: s3tests_conf is {s3cfg}".format(s3cfg = s3tests_conf))
             for section, user in users.items():
                 # log.debug('S3 Tests Go: Setion, User = {sect}, {user}'.format(sect=section, user=user))
                 # self._config_user(s3tests_conf=s3tests_conf[client], section=section, user='{user}.{client}'.format(user=user, client=client))
-                # log.info("S3 Tests Go: s3tests_conf is {s3cfg}".format(s3cfg=s3tests_conf))
+                # log.info("S3 Tests Go: s3tests_conf is {s3cfg}"s.format(s3cfg=s3tests_conf))
                 log.debug('S3 Tests Go: Creating user {user} on {client}'.format(user=user, client=client))
                 cluster_name, daemon_type, client_id = teuthology.split_role(client)
                 client_with_id = daemon_type + '.' + client_id
