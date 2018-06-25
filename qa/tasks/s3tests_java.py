40c94d68891e74bbdb3e0206a45e8a2da28ae7d7
Antoaneta Damyanova <adamyanova@gmail.com>
2018-06-25 11:55:06 +0200

    keytool alias cert import
    Signed-off-by: Antoaneta Damyanova <adamyanova@gmail.com>

--- qa/tasks/s3tests_java.py
+++ qa/tasks/s3tests_java.py
@@ -93,10 +93,11 @@ class S3tests_java(Task):
             stdout=StringIO()
         )
         username = getpass.getuser()
+        endpoint = self.ctx.rgw.role_endpoints.get(client)
         self.ctx.cluster.only(client).run(
-            args=['sudo', 'keytool -import -alias {alias}', 
-            ' -keystore /usr/lib/jvm/java-1.8.0-openjdk-1.8.0.171-8.b10.el7_5.x86_64/jre/lib/security/cacerts',
-            '-file /home/{username}/cephtest/ca/rgw.{client}.crt'.format(username = username, client = client),
+            args=['sudo', 'keytool', '-import', '-alias {alias}'.format(alias = endpoint.hostname), 
+            ' -keystore', '/usr/lib/jvm/java-1.8.0-openjdk-1.8.0.171-8.b10.el7_5.x86_64/jre/lib/security/cacerts',
+            '-file', '/home/{username}/cephtest/ca/rgw.{client}.crt'.format(username = username, client = client),
             '-storepass "changeit"'
             ],
              stdout=StringIO()
