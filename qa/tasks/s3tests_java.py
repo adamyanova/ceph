190447175a1cc9bc4afb247e8e9a6e0ef2f22606
Antoaneta Damyanova <adamyanova@gmail.com>
2018-06-25 13:05:01 +0200

    try keytool
    Signed-off-by: Antoaneta Damyanova <adamyanova@gmail.com>

--- qa/tasks/s3tests_java.py
+++ qa/tasks/s3tests_java.py
@@ -95,7 +95,12 @@ class S3tests_java(Task):
         username = getpass.getuser()
         endpoint = self.ctx.rgw.role_endpoints.get(client)
         self.ctx.cluster.only(client).run(
-            args=['sudo keytool -import -alias {alias} -keystore /usr/lib/jvm/java-1.8.0-openjdk-1.8.0.171-8.b10.el7_5.x86_64/jre/lib/security/cacerts -file /home/ubuntu/cephtest/ca/rgw.{client}.crt  -storepass "changeit"'.format(alias = endpoint.hostname, client = client)
+            args=['sudo',
+            'keytool',
+            '-import', '-alias', '{alias}'.format(alias = endpoint.hostname),
+            '-keystore', '/usr/lib/jvm/java-1.8.0-openjdk-1.8.0.171-8.b10.el7_5.x86_64/jre/lib/security/cacerts',
+            '-file', '/home/ubuntu/cephtest/ca/rgw.{client}.crt'.format(client = client), 
+            '-storepass', 'changeit',
             ],
              stdout=StringIO()
         )
